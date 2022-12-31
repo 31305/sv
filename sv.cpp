a6e7c8034c7bf2371e796a7c9c07492e2c291908
- <>
2022-12-31 13:17:41 +0530

    -

--- sv.cpp
+++ sv.cpp
@@ -145,7 +145,7 @@ unsigned char tns(KeySym t)
 	if(t==XK_KP_9)return 9;
 	return 0;
 }
-const double nv=1?0.1:GS_VTM5_MIN_RADIUS;
+const double nv=0?0.1:GS_VTM5_MIN_RADIUS;
 const double sgvv=0.1;
 double vm(const v& dv,short vk,bool db=0)
 {
@@ -683,11 +683,14 @@ void k(int p,bool lp=0)
 								ps(svk[k],svm(pv,k),vd,1,0,1,1.0/dm/
 										(pv0.vv==5?0.3
 										 :pv0.vv==3?0.3
+										 :pv0.vv==1?0.6
+										 :pv0.vv==4?(km<dm*m1?0.6:0.3)
 										 :0.4));
 								if(0)ps(svk[k],svm(pv,k),dm*m2,
 										pv.vv==3?2
 										:pv.vv==5?2
 										:pv.vv==4?0.5
+										:pv.vv==1?0.5
 										:1);
 							}
 						auto gr=[](const v& dv)
@@ -743,8 +746,8 @@ void k(int p,bool lp=0)
 						if(pv.sv||pv.nt||pv.n||pv.sg)
 							ps(mt.PARAM_GLOT_VOL,pv.cs==v::csp::od?54:60,(!dv&&pv0.mp)?dm*m2:dm*m1,1,(!dv&&pv0.mp)?dm*m1:0);
 						if(nv)ps(mt.PARAM_GLOT_VOL,0,vd,1,vd-dm*m1);
-						if(pv.vv&&!pv.n&&pv.vv!=1&&pv.vv!=4&&pv.vv!=2&&!(!nv&&pv1.vv==pv.vv)&&!nv)
-							ps(mt.PARAM_VB,pv.n?2:pv.vv==5?5:3,vd,1,vd-dm*m1);
+						if(pv.vv&&pv.vv!=1&&pv.vv!=2&&!(!nv&&pv1.vv==pv.vv)&&!nv)
+							ps(mt.PARAM_VB,pv.n?5:15,vd,1,vd-dm*m1);
 						ps(mt.PARAM_VB,0,dm*m1,1);
 						if(pv.sm&&!(pv.cs==v::csp::k))
 						{
@@ -797,7 +800,7 @@ void k(int p,bool lp=0)
 						{
 							double dk=dm*m1;
 							double g=km<dk?(km/dk):(vd-km<dk)?((vd-km)/dk):1;
-							double rvm=0.9*g*(1.2+sin(2*M_PI*km/vd*1.1))*0.5;
+							double rvm=0.9*g*(1.2+sin(2*M_PI*km/dm*2))*0.5;
 							rvm=std::min(rvm,ms[1][mt.PARAM_R7]);
 							bool br=1;
 							if(0)printf("%lf\n",g);
