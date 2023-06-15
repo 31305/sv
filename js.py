d2f706ea5c88f60cdfdaec58539aac8491225621
- <>
2023-06-15 14:19:33 +0530

    -

--- js.py
+++ js.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import sys
 from os import system
-if len(sys.argv)<2:
-    print('!')
-else:
-    system('cp 5.html '+sys.argv[1]+'/index.html')
-    system('cp sv.wasm '+sys.argv[1])
-    system('cp sv*js '+sys.argv[1])
-    system('cp sks.js '+sys.argv[1])
+ls='rs'
+system('mkdir -p '+ls)
+system('cp 5.html '+ls+'/index.html')
+system('cp sv.wasm '+ls)
+system('cp sv*js '+ls)
+system('cp sks.js '+ls)
+system('cp sv.data '+ls)
