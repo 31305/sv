a766cd6939b398c4bb0551839cef4054be2ac633
- <>
2023-06-19 09:42:51 +0530

    -

--- ck.sh
+++ ck.sh
@@ -1,1 +1,1 @@
-hb-view -u '1102f 11038 11018 11013 1102b 11046' --margin="4 4 4 4" --foreground=000000 --background=FFFFFF -o sc.png --font-size 16 /usr/share/fonts/truetype/noto/NotoSansBrahmi-Regular.ttf 
+hb-view -u '1102b 11038 11026 11046 11022 1102f 1102b 11046' --margin="4 4 4 4" --foreground=000000 --background=FFFFFF -o sc.png --font-size 16 /usr/share/fonts/truetype/noto/NotoSansBrahmi-Regular.ttf 
