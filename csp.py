d90d2f8d9d64664155336fb428043b348e0db508
- <>
2023-07-26 10:44:12 +0530

    -

--- csp.py
+++ csp.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import math
 dtnt=0
-for k in range(math.floor(760/8),math.floor(1080/8)):
+for k in range(760,1080):
     ntk=1
     ntks=0
     for pk in range(28,33):
-        km=(k%pk)/k
+        km=(k%(pk*8))/k
         if km<ntk:
             ntk=km
             ntks=pk
-    print(k,ntks,ntk,math.floor(k/ntks))
+    print(k,ntks,ntk,math.floor(k/(ntks*8)))
     if ntk>dtnt:
         dtnt=ntk
 print("\n"+str(dtnt))
