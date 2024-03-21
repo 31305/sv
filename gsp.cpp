bc8c88ccdfda958326e71461d6a51c64a13a0575
- <>
2024-03-21 16:37:11 +0530

    -

--- gsp.cpp
+++ gsp.cpp
@@ -7,7 +7,7 @@ int main()
 	std::time_t k=std::time(0);
 	std::tm* tk=std::gmtime(&k);
 	swe_utc_to_jd(1900+tk->tm_year,1+tk->tm_mon,tk->tm_mday,tk->tm_hour,tk->tm_min,tk->tm_sec,1,tp,ps);
-	swe_pheno_ut(tp[1],1,0,tp,ps);
-	printf("cs %lf\n",tp[1]);
+	swe_pheno_ut(tp[1]+8,SE_MOON,0,tp,ps);
+	printf("cs %lf %lf\n",tp[1],swe_difdegn(SE_MOON,SE_SUN));
 	return 0;
 }
