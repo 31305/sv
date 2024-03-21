18d803a3057488ea8d87de1fa5e26c47b6b5557a
- <>
2024-03-21 15:52:18 +0530

    -

--- gsp.cpp
+++ gsp.cpp
@@ -1,8 +1,11 @@
+#include<chrono>
 #include</usr/include/swephexp.h>
 int main()
 {
 	double tp[20];
 	char ps[1024];
+	std::time_t k=std::time(0);
+	[[maybe_unused]]std::tm* tk=std::gmtime(&k);
 	double sk=2460390.78194;
 	swe_pheno_ut(sk+4,1,0,tp,ps);
 	printf("cs %lf\n",tp[1]);
