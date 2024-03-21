0d782646bc55b895c7fe03e9a981316451323e77
- <>
2024-03-21 16:17:10 +0530

    -

--- gsp.cpp
+++ gsp.cpp
@@ -5,9 +5,9 @@ int main()
 	double tp[20];
 	char ps[1024];
 	std::time_t k=std::time(0);
-	[[maybe_unused]]std::tm* tk=std::gmtime(&k);
-	double sk=2460390.78194;
-	swe_pheno_ut(sk+4,1,0,tp,ps);
+	std::tm* tk=std::gmtime(&k);
+	swe_utc_to_jd(1900+tk->tm_year,1+tk->tm_mon,tk->tm_mday,tk->tm_hour,tk->tm_min,tk->tm_sec,1,tp,ps);
+	swe_pheno_ut(tp[1],1,0,tp,ps);
 	printf("cs %lf\n",tp[1]);
 	return 0;
 }
