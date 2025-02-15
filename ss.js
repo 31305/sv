a6dc9d331b57a1be7b3b69cf5886204c9dc10e92
- <>
2025-02-15 17:51:18 +0530

    -

--- ss.js
+++ ss.js
@@ -19,26 +19,28 @@ function mk()
 
 function pk(m,kn)
 {
-	m.respondWith(caches.open(kn).then(k=>{return k.match(m.request).then(p=>{
+	return caches.open(kn).then(k=>{return k.match(m.request).then(p=>{
 		if(p){return p;};
 		return fetch(m.request).then(p=>{return k.put(m.request,p.clone()).then(()=>{return p;});});
-	})}))
+	})})
 }
 
 
 self.addEventListener('fetch',m=>{
 	if(m.request.url.startsWith(self.location.origin))
 	{
-		if(1)pk(m,nh)
-		else if(m.request.url.length<=self.location.origin.length+1||m.request.url.endsWith('html'))
+		if(m.request.url.length<=self.location.origin.length+1||m.request.url.endsWith('html'))
 		{
-			fetch(new Request(self.location.origin+'sks',{method:"GET"}))
-			.then(p=>{
-				ph=p.text().then(p=>{ph=p})
-			})
-			.catch(pk(m,ph))
+			m.respondWith(fetch(new Request(self.location.origin+'/sks',{method:"GET"}))
+				.then(p=>{
+					if(p.status==404)return pk(m,ph);
+					mk();
+					return p.text().then(p=>{ph=p;return pk(m,ph)})
+				})
+				.catch(()=>pk(m,ph))
+			)
 		}
-		else if(m.request.url.includes('/sv.'))pk(m,ph);
-		else pk(m,nh);
+		else if(!m.request.url.endsWith('mp4'))m.respondWith(pk(m,ph));
+		else m.respondWith(pk(m,nh));
 	}
 });
