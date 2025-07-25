18db93880c95a58122470c7d5083fcdbd1f0c59f
- <>
2025-07-25 13:41:19 +0530

    -

---
+++ 2L/js.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+from os import system
+ls='../../dlrs'
+system('mkdir -p '+ls)
+system('cp 2L.html '+ls)
+system('cp ss.js '+ls)
+system('cp 2L.svg '+ls)
+system('cp sks '+ls)
+system('cp 2L.json '+ls)
